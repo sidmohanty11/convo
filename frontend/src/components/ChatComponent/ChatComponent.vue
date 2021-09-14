3e092c4440da29673ccddc35f8ddc511e38aa091
sidmohanty11 <sidmohanty11@gmail.com>
2021-09-14 21:12:45 +0530

    working on the home page

---
+++ frontend/src/components/ChatComponent/ChatComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <img class="user-image" :src="imageUrl" />
+    <button class="chat-component">@{{ username }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      required: true,
+    },
+    imageUrl: {
+      default: "",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  max-height: 57px;
+  margin: 10px;
+  padding: 10px;
+  width: 90%;
+  border-radius: 7px;
+  display: flex;
+  background-color: #222;
+}
+
+.chat-component {
+  border: none;
+  width: 76%;
+  font-size: 20px;
+  color: whitesmoke;
+  background-color: transparent;
+}
+.user-image {
+  max-height: 50px;
+  width: 50px;
+  border-radius: 100%;
+}
+</style>
---
+++ frontend/src/views/Message.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>This is message page{{ paramId }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      paramId: this.$route.params.id,
+    };
+  },
+};
+</script>
